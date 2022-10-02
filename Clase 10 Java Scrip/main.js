178ea84a51d3849e16c2ecced592c0d3e1533ee0
Norma Pintos <normabeatrizp@gmail.com>
2022-10-01 22:20:54 -0300

    preparando

---
+++ Clase 10 Java Scrip/main.js
@@ -0,0 +1,14 @@
+console.log("Hola mundo desde archivo externo")
+//
+let nombre="Norma";
+console.log(nombre)
+let apellido="Pintos";
+console.log(apellido)
+let edad = 40;
+console.log(edad)
+let mascota= true
+console.log(mascota)
+
+
+
+34
